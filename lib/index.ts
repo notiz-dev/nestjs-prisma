5b6bb62dded333c5a45b574d96b0b8324120c588
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-12-15 13:40:26 +0100

    expose prisma constants

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,4 @@ export * from './prisma.module';
 export * from './prisma.service';
 export * from './prisma-client-exception.filter';
 export * from './interfaces';
+export * from './prisma.constants';
