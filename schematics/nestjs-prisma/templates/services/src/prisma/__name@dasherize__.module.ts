f4491da7443e4bd782d6e28ce6a546b467e421fb
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2022-01-04 12:25:15 +0100

    add global module link for schematics

--- schematics/nestjs-prisma/templates/services/src/prisma/__name@dasherize__.module.ts
+++ schematics/nestjs-prisma/templates/services/src/prisma/__name@dasherize__.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { <%= classify(name) %>Service } from './<%= dasherize(name) %>.service';
 
+/**
+ * See optional instruction for global modules https://docs.nestjs.com/modules#global-modules
+ */
 @Module({
   providers: [<%= classify(name) %>Service],
   exports: [<%= classify(name) %>Service],
