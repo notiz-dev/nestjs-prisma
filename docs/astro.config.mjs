e371ede9d9b9f03e4d82ab56bde133178bc696aa
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2022-10-05 14:11:38 +0200

    add sitemap

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nestjs-prisma.dev',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 });
