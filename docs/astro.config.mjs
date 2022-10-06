b764e00a58cd01ed19b2a57d0c9fb64b8b172135
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2022-10-06 14:48:52 +0200

    add mobile menu with alpinejs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from '@astrojs/sitemap';
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nestjs-prisma.dev',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), alpinejs()]
 });
