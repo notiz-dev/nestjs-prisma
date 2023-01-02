addac1d3f2de909feae525c9a0856daffedb3c45
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2023-01-02 18:56:36 +0100

    add mobile search button

--- docs/src/components/Search.tsx
+++ docs/src/components/Search.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource react */
-import { useState, useCallback, useRef } from 'react';
+import { useState, useCallback, useRef, useEffect } from 'react';
 import { ALGOLIA } from '../config';
 import '@docsearch/css';
 import './Search.css';
@@ -9,86 +9,116 @@ import * as docSearchReact from '@docsearch/react';
 
 /** FIXME: This is still kinda nasty, but DocSearch is not ESM ready. */
 const DocSearchModal =
-	docSearchReact.DocSearchModal || (docSearchReact as any).default.DocSearchModal;
+  docSearchReact.DocSearchModal ||
+  (docSearchReact as any).default.DocSearchModal;
 const useDocSearchKeyboardEvents =
-	docSearchReact.useDocSearchKeyboardEvents ||
-	(docSearchReact as any).default.useDocSearchKeyboardEvents;
+  docSearchReact.useDocSearchKeyboardEvents ||
+  (docSearchReact as any).default.useDocSearchKeyboardEvents;
 
-export default function Search() {
-	const [isOpen, setIsOpen] = useState(false);
-	const searchButtonRef = useRef<HTMLButtonElement>(null);
-	const [initialQuery, setInitialQuery] = useState('');
+export function SearchIcon(props: any) {
+  return (
+    <svg width="24" height="24" fill="none" {...props}>
+      <path
+        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
 
-	const onOpen = useCallback(() => {
-		setIsOpen(true);
-	}, [setIsOpen]);
+export function Search() {
+  let [modifierKey, setModifierKey] = useState<string>('⌘');
+  const [isOpen, setIsOpen] = useState(false);
+  const searchButtonRef = useRef<HTMLButtonElement>(null);
+  const [initialQuery, setInitialQuery] = useState('');
 
-	const onClose = useCallback(() => {
-		setIsOpen(false);
-	}, [setIsOpen]);
+  useEffect(() => {
+    setModifierKey(
+      /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform) ? '⌘' : 'Ctrl '
+    );
+  }, []);
 
-	const onInput = useCallback(
-		(e: any) => {
-			setIsOpen(true);
-			setInitialQuery(e.key);
-		},
-		[setIsOpen, setInitialQuery]
-	);
+  const onOpen = useCallback(() => {
+    setIsOpen(true);
+  }, [setIsOpen]);
 
-	useDocSearchKeyboardEvents({
-		isOpen,
-		onOpen,
-		onClose,
-		onInput,
-		searchButtonRef,
-	});
+  const onClose = useCallback(() => {
+    setIsOpen(false);
+  }, [setIsOpen]);
 
-	return (
-		<>
-		{/* TODO show only search icon on small displays */}
-			<button type="button" ref={searchButtonRef} onClick={onOpen} className="w-60 flex items-center text-sm leading-6 text-slate-400 rounded-md ring-1 ring-gray-900/10 shadow-sm py-1.5 pl-2 pr-3 hover:ring-gray-300 dark:bg-gray-800 dark:highlight-white/5 dark:hover:bg-gray-700">
-				<svg width="24" height="24" fill="none" className="mr-3">
-					<path
-						d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-						stroke="currentColor"
-						strokeWidth="2"
-						strokeLinecap="round"
-						strokeLinejoin="round"
-					/>
-				</svg>
+  const onInput = useCallback(
+    (e: any) => {
+      setIsOpen(true);
+      setInitialQuery(e.key);
+    },
+    [setIsOpen, setInitialQuery]
+  );
 
-				<span>Search...</span>
+  useDocSearchKeyboardEvents({
+    isOpen,
+    onOpen,
+    onClose,
+    onInput,
+    searchButtonRef,
+  });
 
-				<span className="ml-auto">
-				⌘K
-				</span>
-			</button>
+  return (
+    <>
+      <button
+        type="button"
+        ref={searchButtonRef}
+        onClick={onOpen}
+        className="dark:highlight-white/5 hidden w-60 items-center gap-2 rounded-md py-1.5 pl-2 pr-3 text-sm leading-6 text-slate-400 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700 md:flex"
+      >
+        <SearchIcon />
+        <span>Search docs...</span>
 
-			{isOpen &&
-				createPortal(
-					<DocSearchModal
-						initialQuery={initialQuery}
-						initialScrollY={window.scrollY}
-						onClose={onClose}
-						indexName={ALGOLIA.indexName}
-						appId={ALGOLIA.appId}
-						apiKey={ALGOLIA.apiKey}
-						transformItems={(items) => {
-							return items.map((item) => {
-								// We transform the absolute URL into a relative URL to
-								// work better on localhost, preview URLS.
-								const a = document.createElement('a');
-								a.href = item.url;
-								const hash = a.hash === '#overview' ? '' : a.hash;
-								return {
-									...item,
-									url: `${a.pathname}${hash}`,
-								};
-							});
-						}}
-					/>,
-					document.body
-				)}
-		</>
-	);
+        <span className="ml-auto">{modifierKey}K</span>
+      </button>
+
+      {isOpen &&
+        createPortal(
+          <DocSearchModal
+            initialQuery={initialQuery}
+            initialScrollY={window.scrollY}
+            onClose={onClose}
+            indexName={ALGOLIA.indexName}
+            appId={ALGOLIA.appId}
+            apiKey={ALGOLIA.apiKey}
+            transformItems={(items) => {
+              return items.map((item) => {
+                // We transform the absolute URL into a relative URL to
+                // work better on localhost, preview URLS.
+                const a = document.createElement('a');
+                a.href = item.url;
+                const hash = a.hash === '#overview' ? '' : a.hash;
+                return {
+                  ...item,
+                  url: `${a.pathname}${hash}`,
+                };
+              });
+            }}
+          />,
+          document.body
+        )}
+    </>
+  );
+}
+
+export function MobileSearch() {
+  return (
+    <div className="block md:hidden">
+      <button
+        type="button"
+        className="flex h-6 w-6 items-center justify-center rounded-md"
+        aria-label="Search docs..."
+      >
+        <SearchIcon className="h-5 w-5" />
+      </button>
+      {/* TODO search dialog */}
+    </div>
+  );
 }
