738c879920e37b30c88ec9d3afc01da9c263b56c
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2023-01-26 13:54:20 +0100

    use enableShutdownHooks

--- examples/extensions/src/main.ts
+++ examples/extensions/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CustomPrismaService } from 'nestjs-prisma';
+import { extendedPrismaClient } from './prisma.extension';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const customPrismaService: CustomPrismaService<extendedPrismaClient> =
+    app.get('PrismaService');
+  await customPrismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
 }
 bootstrap();
