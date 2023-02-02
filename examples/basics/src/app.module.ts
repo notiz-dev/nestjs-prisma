c36e482cb3ede55ad38a2bb67651bab8a3a02ca6
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2023-02-02 10:45:28 +0100

    update examples deps

--- examples/basics/src/app.module.ts
+++ examples/basics/src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from 'nestjs-prisma';
+import { APP_FILTER, HttpAdapterHost } from '@nestjs/core';
+import { PrismaModule, PrismaClientExceptionFilter } from 'nestjs-prisma';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [PrismaModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // here or in main.ts
+    // {
+    //   provide: APP_FILTER,
+    //   useFactory: ({ httpAdapter }: HttpAdapterHost) => {
+    //     return new PrismaClientExceptionFilter(httpAdapter);
+    //   },
+    //   inject: [HttpAdapterHost],
+    // },
+  ],
 })
 export class AppModule {}
