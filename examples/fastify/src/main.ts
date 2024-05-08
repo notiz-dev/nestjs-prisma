c7dd78b54a9ddd4252efe6414065370d5b7ef882
Steven Eubank <47563310+smeubank@users.noreply.github.com>
2024-05-08 13:41:22 +0000

    add sentry

--- examples/fastify/src/main.ts
+++ examples/fastify/src/main.ts
@@ -1,12 +1,18 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { BaseExceptionFilter, HttpAdapterHost, NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 import { AppModule } from './app.module';
+import * as Sentry from '@sentry/node';
 
 async function bootstrap() {
+  Sentry.init({ 
+      dsn: "YOUR_DSN", 
+      tracesSampleRate: 1.0,
+      debug: true, });
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
@@ -15,6 +21,7 @@ async function bootstrap() {
   // prisma exception filter
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+  Sentry.setupNestErrorHandler(app, new BaseExceptionFilter(httpAdapter));
 
   await app.listen(3000);
 }
