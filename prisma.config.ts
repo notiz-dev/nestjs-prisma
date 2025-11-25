0032fc2e68813b9bdaefe64f2ceaf9fb5b139225
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-11-25 11:46:31 +0100

    update library to use prisma 7

---
+++ prisma.config.ts
@@ -0,0 +1,13 @@
+import path from 'node:path';
+import { defineConfig } from 'prisma/config';
+
+export default defineConfig({
+  schema: path.join('prisma', 'schema.prisma'),
+  migrations: {
+    path: './prisma/migrations',
+    seed: 'tsx ./prisma/seed.ts',
+  },
+  datasource: {
+    url: 'file:./dev.db',
+  },
+});
