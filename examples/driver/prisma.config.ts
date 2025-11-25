0ec94bff13f8439bf84b9caea1bbc1e9d31a14d6
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-11-25 12:11:58 +0100

    update driver example to prisma v7 and nestjs-prisma@0.27.0-dev.0

--- examples/driver/prisma.config.ts
+++ examples/driver/prisma.config.ts
@@ -7,4 +7,7 @@ export default defineConfig({
     path: './prisma/migrations',
     seed: 'tsx ./prisma/seed.ts',
   },
+  datasource:  {
+    url: 'file:./prisma/dev.db',
+  }
 });
--- examples/driver/src/prisma.extension.ts
+++ examples/driver/src/prisma.extension.ts
@@ -1,7 +1,7 @@
-import { PrismaBetterSQLite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaClient } from './generated/prisma/client';
 
-const adapter = new PrismaBetterSQLite3({ url: 'file:./prisma/dev.db' });
+const adapter = new PrismaBetterSqlite3({ url: 'file:./prisma/dev.db' });
 export const prisma = new PrismaClient({ adapter });
 
 export type PrismaClientType = typeof prisma;
