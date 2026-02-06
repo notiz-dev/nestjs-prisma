16092cdf61fb450b318c65165cd2830883fa68a0
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-02-06 12:09:41 +0100

    add analytics

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nestjs-prisma.dev',
+
   integrations: [
     starlight({
       title: 'nestjs-prisma',
@@ -16,11 +17,26 @@ export default defineConfig({
         },
       ],
       sidebar: [
-		{
+        {
           label: 'Getting Started',
           autogenerate: { directory: 'getting-started' },
         },
       ],
+      head: [
+        {
+          tag: 'script',
+          attrs: {
+            async: true,
+            src: 'https://analytics.notiz.dev/js/pa-Tcb4hLre87BwyFcqW-0yZ.js',
+          },
+        },
+        {
+          tag: 'script',
+          content: `window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+plausible.init()`,
+        },
+      ],
+
     }),
   ],
 });
